30635e126a2bd49c1a7e6245ef4c3faa65e97221
Sahlath <118336998+Sahlath@users.noreply.github.com>
2022-11-23 13:40:06 +0530

    Module 2 chapter 5

--- learning-go-2875237/practice/main.go
+++ learning-go-2875237/practice/main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
-	fmt.Println("Hello from Go!")
+
+	i1, i2, i3 := 12, 45, 68
+	intSum := i1 + i2 + i3
+	fmt.Println("Integer sum:", intSum)
+
+	f1, f2, f3 := 23.5, 65.1, 76.3
+	floatSum := f1 + f2 + f3
+	fmt.Println("Float sum:", floatSum)
+
+	roundedSum := math.Round(floatSum)
+	fmt.Println("Rounded sum:", roundedSum)
+
 }
