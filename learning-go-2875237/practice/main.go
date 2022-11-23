f18bdf2ee7c7d70032b92e35251b9a9ac759d70d
Sahlath <118336998+Sahlath@users.noreply.github.com>
2022-11-23 13:38:03 +0530

    Module 2 chapter 4

--- learning-go-2875237/practice/main.go
+++ learning-go-2875237/practice/main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello from Go!")
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text: ")
+	input, _ := reader.ReadString('\n')
+	fmt.Println("You entered:", input)
+
+	fmt.Print("Enter a number: ")
+	inputNum, _ := reader.ReadString('\n')
+	convertedFloat, err := strconv.ParseFloat(strings.TrimSpace(inputNum), 64)
+	if err != nil {
+		fmt.Println("Its an error:", err)
+	} else {
+		fmt.Println("The number entered is:", convertedFloat)
+	}
+
 }
