6632386a5a274dd4606de7cbfbe17c4d278a6f08
Sahlath <118336998+Sahlath@users.noreply.github.com>
2022-11-23 13:44:38 +0530

    module 2 chapter 7

--- learning-go-2875237/practice/main.go
+++ learning-go-2875237/practice/main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
-	fmt.Println("Hello from Go!")
+
+	n := time.Now()
+	fmt.Println("Current time is:", n)
+
+	joiningDate := time.Date(2022, time.November, 8, 11, 0, 0, 0, time.UTC)
+	fmt.Println("Joining date is \n", joiningDate)
+	fmt.Printf("Joining date is %T\n", joiningDate)
+	fmt.Println("Formatted date is \n", joiningDate.Format(time.ANSIC))
+
 }
