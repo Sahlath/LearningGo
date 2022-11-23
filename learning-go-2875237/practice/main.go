8694316dc40c6db78a9fa0bc2337199a9c6396e8
Sahlath <118336998+Sahlath@users.noreply.github.com>
2022-11-23 13:33:07 +0530

    module 2 chapter 2

--- learning-go-2875237/practice/main.go
+++ learning-go-2875237/practice/main.go
@@ -5,5 +5,27 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello from Go!")
+	var aString string = "This is GO!"
+	fmt.Println(aString)
+	fmt.Printf("The variable type is %T\n", aString)
+	var anotherString = "I am another String"
+	fmt.Println(anotherString)
+	fmt.Printf("The variable type is %T\n", anotherString)
+
+	var anInteger int = 40
+	fmt.Println(anInteger)
+	fmt.Printf("The variable type is %T\n", anInteger)
+
+	var anotherInteger float64 = 40.0
+	fmt.Println(anotherInteger)
+	fmt.Printf("The variable type is %T\n", anotherInteger)
+
+	alsoString := "I am also a string"
+	fmt.Println(alsoString)
+	fmt.Printf("The variable type is %T\n", alsoString)
+
+	var defaultInt int
+	fmt.Println(defaultInt)
+	fmt.Printf("The defaultInt variable type is %T\n", defaultInt)
+
 }
