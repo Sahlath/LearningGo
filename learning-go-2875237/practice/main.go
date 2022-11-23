880addb829a02b49ea6ab40687938ac94735fc74
Sahlath <118336998+Sahlath@users.noreply.github.com>
2022-11-23 13:43:30 +0530

    Module 2 chapter 6

--- learning-go-2875237/practice/main.go
+++ learning-go-2875237/practice/main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
-	fmt.Println("Hello from Go!")
+
+	i1, i2, i3 := 20, 30, 20
+	sum := i1 + i2 + i3
+	fmt.Println("Sum is :", sum)
+
+	f1, f2, f3 := 23.7, 24.5, 12.9
+	sumFloat := f1 + f2 + f3
+	fmt.Println("sumFloat is :", sumFloat)
+
+	sumFloat = math.Round(sumFloat)
+	fmt.Println("sumFloat rounded is :", sumFloat)
+
+	circleRadius := 12.5
+	circumference := circleRadius * 2 * math.Pi
+	fmt.Printf("circumefernce is %.3f\n", circumference)
 }
